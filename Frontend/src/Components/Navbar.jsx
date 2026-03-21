4503d4e585ed0b73668b98b8e99fd57592f1e8c3
NormieCodes <rishisagaram5@gmail.com>
2026-03-22 03:28:52 +0530

    on page routing

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar () {
   return (
     <>
-      <nav className="flex justify-between items-center p-4 bg-green-800 border-b-2 border-b-black">
-        <h1 className="font-extrabold font-mono text-xl cursor-pointer">Simply South</h1>
+      <nav className="flex justify-between items-center p-4 bg-green-800 border-b-2 border-b-black fixed z-50 w-full">
+        <h1 className="font-extrabold font-mono text-xl cursor-pointer"><a href="#home">Simply South</a></h1>
 
         <ul className="flex gap-6 font-semibold items-center">
           <li><a className='hover:bg-green-500 py-2 px-3 rounded-md duration-200' href="#home">
