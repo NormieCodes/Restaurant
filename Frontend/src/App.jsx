be300fe0778043b9067ad6ac8caed4f46a55b3d7
NormieCodes <rishisagaram5@gmail.com>
2026-03-22 03:31:03 +0530

    add Contact section and Footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,9 @@ import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
 import About from './Components/About.jsx'
 import Menu from './Components/Menu.jsx'
+import Contact from './Components/Contact.jsx'
+import Footer from './Components/Footer.jsx'
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
     <Hero />
     <About />
     <Menu />
+    <Contact />
+    <Footer />
     </>
   );
 }
