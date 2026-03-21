2670ef3bce38a0f85bc0cc4bb4684492e9f59306
NormieCodes <rishisagaram5@gmail.com>
2026-03-22 00:17:11 +0530

    add menu section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
+import About from './Components/About.jsx'
+import Menu from './Components/Menu.jsx'
 
 function App() {
   return (
     <>
     <Navbar />
     <Hero />
+    <About />
+    <Menu />
     </>
   );
 }
