0f9325bb58961dc2b330e2a69b94b4f421b2d17f
NormieCodes <rishisagaram5@gmail.com>
2026-03-22 03:29:21 +0530

    basic footer

---
+++ Frontend/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <>
+      <div className="w-full h-8 bg-green-900 flex justify-center items-center inset-shadow-md ">
+        <p className="text-center font-mono text-white text-md">
+          © 2025 Simply South. All rights reserved.
+        </p>
+      </div>
+    </>
+  );
+} 
+
+export default Footer;
