e79c647b4930fa7813d02a9c26fc6c978481d6fe
NormieCodes <rishisagaram5@gmail.com>
2026-03-22 00:54:35 +0530

    optimized menu card rendering

--- Frontend/src/Components/Menu.jsx
+++ Frontend/src/Components/Menu.jsx
@@ -9,6 +9,17 @@ import thali from '../assets/thali-banner.jpg'
 import menu from '../assets/menu-bg.jpg'
 
 
+const menuCategories = [
+  {name: "Idli and Vada", image: iv},
+  {name: "Dosa", image: dosa},
+  {name: "Lights Eats", image: upma},
+  {name: "Rice Items", image: rice},
+  {name: "Deserts", image: desert},
+  {name: "Beverages", image: beverages},
+  {name: "Chinese (Angara Grill)", image: chinese},
+  {name: "Combos", image: thali}
+];
+
 function Menu() {
   return (
     <>
@@ -28,70 +39,19 @@ function Menu() {
 
         {/* Menu Container */}
         <div className="grid gap-4 max-w-6xl mx-auto px-6 py-4 my-4 grid-cols-3 ">
-          {/* Dish Cards */}
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={iv} 
-            alt="iv" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Idli and Vada</p>
-          </div>
-          
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={dosa} 
-            alt="dosa" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Dosa</p>
-          </div>
 
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={upma} 
-            alt="upma" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Light Eats</p>
-          </div>
-          
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={rice} 
-            alt="rice" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Rice Items</p>
-          </div>
-          
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={desert} 
-            alt="desert" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Deserts</p>
-          </div>
+          {/* Menu Item Cards */}
+          {menuCategories.map((item) => (
+            <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg" key={item.name}>
 
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={beverages} 
-            alt="beverages" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Beverages</p>
-          </div>
+              <img className='object-cover h-[120px] w-full rounded-lg' src={item.image} alt={item.name} />
 
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={chinese} 
-            alt="chinese" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Chinese (Angara Grill)</p>
-          </div>
+              <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
 
-          <div className="flex flex-col justify-center items-center relative cursor-pointer my-6 shadow-2xl border-black border rounded-lg">
-            <img className='object-cover h-[120px] w-full rounded-lg'
-            src={thali} 
-            alt="thali" />
-            <div className='w-full bg-black absolute h-full bg-opacity-40 hover:bg-opacity-30'></div>
-            <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">Combos</p>
-          </div>
+              <p className="text-center text-md font-mono absolute bottom-2 text-white bg-black bg-opacity-35 px-2 font-semibold rounded-xl">{item.name}</p>
+            </div>
+          ))};
+
         </div>
       </section>
     </>
